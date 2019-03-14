d0d46bf601f7922ae387ee9de47a89fee4fcbce5
jpmantuano <joseph.mantuano@redflaggroup.com>
2019-03-14 17:25:27 +0800

    adds link_filters.yml to configuration folder when running ACHE as a REST server

--- src/main/java/focusedCrawler/link/LinkFilterConfig.java
+++ src/main/java/focusedCrawler/link/LinkFilterConfig.java
@@ -1,7 +1,6 @@
 package focusedCrawler.link;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,24 +11,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 public class LinkFilterConfig {
 
     private static final ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+    private static final String LINK_FILTERS_FILE = "link_filters.yml";
 
     static {
         yamlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     @JsonProperty("global.type")
-    private String type;
+    private String type = "wildcard";
 
     @JsonProperty("global.whitelist")
-    private List<String> whitelist;
+    private List<String> whitelist = Collections.emptyList();
 
     @JsonProperty("global.blacklist")
-    private List<String> blacklist;
+    private List<String> blacklist = Collections.emptyList();
 
     private String fileLocation;
 
@@ -43,13 +44,11 @@ public class LinkFilterConfig {
     public LinkFilterConfig(Path linkFiltersPath) {
         Path linkFiltersFile;
         if (Files.isDirectory(linkFiltersPath)) {
-            linkFiltersFile = linkFiltersPath.resolve("link_filters.yml");
+            linkFiltersFile = linkFiltersPath.resolve(LINK_FILTERS_FILE);
         } else {
             linkFiltersFile = linkFiltersPath;
         }
         try {
-
-            LinkFilterConfig linkFilterConfig = yamlMapper.readValue(linkFiltersFile.toFile(), LinkFilterConfig.class);
             init(yamlMapper.readTree(linkFiltersFile.toFile()));
         } catch (IOException e) {
             throw new IllegalArgumentException("Could not read config from file: " + linkFiltersFile.toString(), e);
@@ -72,17 +71,14 @@ public class LinkFilterConfig {
         return blacklist;
     }
 
-    @JsonSetter("global.type")
     public void setLinkFilterType(String type) {
         this.type = type;
     }
 
-    @JsonSetter("global.whitelist")
     public void setWhiteList(List<String> whitelist) {
         this.whitelist = whitelist;
     }
 
-    @JsonSetter("global.blacklist")
     public void setBlackList(List<String> blacklist) {
         this.blacklist = blacklist;
     }
